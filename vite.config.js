edd0002edefe6fb325698795b62f522ec2067b9c
Moises Elias Ramirez Cabrera <141798007+Sesiom31@users.noreply.github.com>
2023-11-29 16:33:46 -0500

    Configuracion para desplegar en github pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: 'https://Sesiom31.github.io/d3-bar-chart',
+});
